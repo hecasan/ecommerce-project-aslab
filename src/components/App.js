06f302177286ee5eb3520142ea5e4874cf5f3b49
Héte Caetano <hetecaetano@gmail.com>
2021-01-29 19:13:00 -0300

    Projeto base inicial

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>ASLab Commerce</h1>
+      <h2>Projeto base de ecommerce para clientes Agência Site</h2>
+    </div>
+  )
+}
